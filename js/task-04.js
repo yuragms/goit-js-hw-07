27e79e06e6964378ed193ee5a79c7f356cf76c76
yuragms <yura.gms@gmail.com>
2021-06-08 15:00:20 +0300

    9

--- js/task-04.js
+++ js/task-04.js
@@ -1,22 +1,44 @@
 const decrementbutton = document.querySelector('button[data-action="decrement"]');
 const incrementbutton = document.querySelector('button[data-action="increment"]');
-let counterValue = document.querySelector('#value').textContent;
+const counterValue = document.querySelector('#value');
 
-let valueContentnum = Number(counterValue);
+
 
 incrementbutton.addEventListener('click', handleIncrementButtonClick);
 decrementbutton.addEventListener('click', handleDecrementButtonClick);
 
 function handleIncrementButtonClick() {
-    
-    valueContentnum += 1;
-    return document.querySelector('#value').textContent = valueContentnum;
+    let counterValueNum = Number(counterValue.textContent);
+    return counterValue.textContent  = counterValueNum + 1;
     
 };
 
 function handleDecrementButtonClick() {
     
-    valueContentnum -= 1;
-    return document.querySelector('#value').textContent = valueContentnum;
+    return counterValue.textContent -= 1;
     
 };
+
+
+// const decrementbutton = document.querySelector('button[data-action="decrement"]');
+// const incrementbutton = document.querySelector('button[data-action="increment"]');
+// let counterValue = document.querySelector('#value').textContent;
+
+// let valueContentnum = Number(counterValue);
+
+// incrementbutton.addEventListener('click', handleIncrementButtonClick);
+// decrementbutton.addEventListener('click', handleDecrementButtonClick);
+
+// function handleIncrementButtonClick() {
+    
+//     valueContentnum += 1;
+//     return document.querySelector('#value').textContent = valueContentnum;
+    
+// };
+
+// function handleDecrementButtonClick() {
+    
+//     valueContentnum -= 1;
+//     return document.querySelector('#value').textContent = valueContentnum;
+    
+// };
--- js/task-06.js
+++ js/task-06.js
@@ -2,6 +2,14 @@ const validationInput = document.querySelector("#validation-input");
 
 validationInput.addEventListener("blur", onInputBlur);
 
+function addClass (x) {
+  validationInput.classList.add(x);
+}
+
+function removeClass (x) {
+  validationInput.classList.remove(x);
+}
+
 function onInputBlur(event) {
   // console.log(validationInput.getAttribute('data-length'));
   // console.log(event.currentTarget.value.length);
@@ -9,10 +17,28 @@ function onInputBlur(event) {
     Number(validationInput.getAttribute("data-length")) ===
     event.currentTarget.value.length
   ) {
-    validationInput.classList.add("valid");
-    validationInput.classList.remove("invalid");
+    addClass('valid');
+    removeClass('invalid');
   } else {
-    validationInput.classList.remove("valid");
-    validationInput.classList.add("invalid");
+    removeClass('valid');
+    addClass('invalid');
   }
 }
+
+
+// validationInput.addEventListener("blur", onInputBlur);
+
+// function onInputBlur(event) {
+//   // console.log(validationInput.getAttribute('data-length'));
+//   // console.log(event.currentTarget.value.length);
+//   if (
+//     Number(validationInput.getAttribute("data-length")) ===
+//     event.currentTarget.value.length
+//   ) {
+//     validationInput.classList.add("valid");
+//     validationInput.classList.remove("invalid");
+//   } else {
+//     validationInput.classList.remove("valid");
+//     validationInput.classList.add("invalid");
+//   }
+// }
